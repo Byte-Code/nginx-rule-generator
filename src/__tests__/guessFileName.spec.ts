cf034d9b194759c9775f8ee3d2e35c904d7ae16b
apalumbo <apalumbo@byte-code.com>
2023-04-04 19:06:37 +0200

    stripped + characted from file name, added test for long urls

--- src/__tests__/guessFileName.spec.ts
+++ src/__tests__/guessFileName.spec.ts
@@ -20,6 +20,30 @@ describe('guessFileName', () => {
       ]
     });
 
-    expect(result).toMatch(/uomo_html_[a-z0-9-]+\.rule/);
+    expect(result).toMatch(/uomo_html_[a-f0-9]+\.rule/);
   })
+
+  it('long url with regexp special chars', () => {
+    const result = guessFileName({
+      baseUrl: '/it/1277-saldi/s-1/taglia-4_anni/brand_2-armani_junior+balmain_kids+calvin_klein_kids+dolce_gabbana_kids+dsquared2+fay_kids+fendi_kids+givenchy_kids+moschino_kids+msgm_kids+ralph_lauren_kids+hugo_boss+diesel/prezzo-15:150/extra_sconto-saldi',
+      variants: [
+        {
+          fullQueryString: undefined,
+          destination: 'https://coccolebimbi.com/it-it/c/saldi'
+        }
+      ]
+    })
+
+    expect(result).toMatch(/it_1277-saldi_s-1_taglia-4_anni_brand_2-armani_junior_balmain_kids_calvin_klein_kids_dolce_gabbana_kids_dsquared2_fay_kids_fendi_kids_givenchy_kids_moschino_kids_msgm_kids_ralph_lauren_kids_hugo_boss__[a-f0-9]+\.rule/);
+  })
+
+
+
+
+
+  ///https://redirects.coccolebimbi.com/it/1277-saldi/s-1/taglia-4_anni/brand_2-armani_junior+balmain_kids+calvin_klein_kids+dolce_gabbana_kids+dsquared2+fay_kids+fendi_kids+givenchy_kids+moschino_kids+msgm_kids+ralph_lauren_kids+hugo_boss+diesel/prezzo-15:150/extra_sconto-saldi,
+  
+  
+  
+  
 })
--- src/file-name-generator.ts
+++ src/file-name-generator.ts
@@ -3,7 +3,7 @@ import { Location } from './Location';
 
 export const guessFileName = (location: Location) => {
   const baseUrlWithoutStartingSlash = location.baseUrl.substr(1);
-  const clearedPath = baseUrlWithoutStartingSlash.replace(/[\/,\.]/g, '_');  
+  const clearedPath = baseUrlWithoutStartingSlash.replace(/[\/,\.\+]/g, '_');  
   const digest = crypto.createHash('sha256').update(clearedPath).digest('hex')
   return `${!clearedPath ? 'home' : clearedPath.substring(0,200)}_${digest}.rule`;
     
